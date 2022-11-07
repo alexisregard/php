0469094cb8118dad46312a2f93ecd6b53177abe0
alexisregard <115563646+alexisregard@users.noreply.github.com>
2022-11-07 13:32:25 +0100

    PHP Lamy

---
+++ emails.php
@@ -0,0 +1,12 @@
+<?php
+
+$emails= [
+    "Julie" => "julie@gmail.com";
+    "Charlotte" => "charlotte@free.fr";
+    "Louis" => "louis@laposte.fr";
+]
+
+foreach($emails as $nom => $email) {
+    echo "Le mail de $nom est $email.";
+
+
